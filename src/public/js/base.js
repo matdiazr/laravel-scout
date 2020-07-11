092f58cf49aa2161f607ebce016443e1fe3175a5
mat.diazr <mat.diazr@gmail.com>
2020-07-11 02:00:56 -0400

    prev instagram api

--- src/public/js/base.js
+++ src/public/js/base.js
@@ -1,1 +1,24 @@
-console.log('hola mundo');
+// arroja un alert con input
+// var usuario = prompt('identifiquese');
+
+var primera, segunda;
+primera = 20;
+segunda = 24;
+var comparacion = segunda > primera; 
+variable = typeof(comparacion);
+
+// console.log(variable);
+
+// challenge 1
+var masa, altura;
+
+masa = 60;
+altura = 1.70;
+mati = bmi = masa / (altura * altura);
+
+masa = 70;
+altura = 1.50;
+cony = bmi = masa / (altura * altura);
+evaluacion = mati > cony;
+console.log('matias: ' + mati + ' , cony: ' + cony );
+console.log('matias tiene mayor bmi que cony ? ' + evaluacion);

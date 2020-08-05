c7602212ac89897d2a9f0567ef0c99f28843ab39
mat.diazr <mat.diazr@gmail.com>
2020-08-05 19:54:55 -0400

    require maatweb

--- src/config/app.php
+++ src/config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Aschmelyun\Larametrics\LarametricsServiceProvider::class,
 
         /*
          * Package Service Providers...
